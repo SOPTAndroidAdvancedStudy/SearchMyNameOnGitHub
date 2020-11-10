ff14aa5d0160b305f65f6ce975927ab0bc9bc2a5
Hyun Woo Lee <l2hyunwoo@gmail.com>
2020-11-10 15:52:56 +0900

    #2 feat: Data binding view and viewmodel

--- app/src/main/java/com/siba/searchmvvmpractice/ui/MainActivity.kt
+++ app/src/main/java/com/siba/searchmvvmpractice/ui/MainActivity.kt
@@ -2,11 +2,22 @@ package com.siba.searchmvvmpractice.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.tabs.TabLayoutMediator
 import com.siba.searchmvvmpractice.R
+import com.siba.searchmvvmpractice.databinding.ActivityMainBinding
+import com.siba.searchmvvmpractice.viewmodel.SearchViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply {
+            searchViewModel = ViewModelProvider.NewInstanceFactory().create(SearchViewModel::class.java)
+            fragmentManager = supportFragmentManager
+            lifecycleOwner = this@MainActivity
+        }
     }
 }
