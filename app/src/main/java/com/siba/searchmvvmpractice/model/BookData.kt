e0ff54ef510583067574645b93cf0f52bea14143
Hyun Woo Lee <l2hyunwoo@gmail.com>
2020-11-13 18:52:58 +0900

    #7 feat: Add live data

---
+++ app/src/main/java/com/siba/searchmvvmpractice/model/BookData.kt
@@ -0,0 +1,7 @@
+package com.siba.searchmvvmpractice.model
+
+data class BookData (
+    val img_book: String,
+    val txt_author: String,
+    val txt_title: String,
+)
--- app/src/main/java/com/siba/searchmvvmpractice/ui/BookSearchFragment.kt
+++ app/src/main/java/com/siba/searchmvvmpractice/ui/BookSearchFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import com.siba.searchmvvmpractice.R
+import com.siba.searchmvvmpractice.databinding.FragmentBookSearchBinding
+import com.siba.searchmvvmpractice.viewmodel.SearchViewModel
 
 class BookSearchFragment : Fragment() {
+    private lateinit var binding: FragmentBookSearchBinding
+    private val searchViewModel: SearchViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_search, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_book_search, container, false)
+        binding.lifecycleOwner = this
+        binding.searchViewModel = searchViewModel
+        return binding.root
     }
 
 }
